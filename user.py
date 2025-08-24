c1e667f70fe920851af7b836616bb3cca83d1549
Liliya <liliyakhu@ukr.net>
2025-08-24 16:10:46 +0300

    Rebasing and fixing conflicts

--- user.py
+++ user.py
@@ -1,9 +1,10 @@
 class User:
     name = "Liliya"
     age = 44
+    image = "some image"
 
     def hello(self):
         print("Hello World!")
 
-    def hi(self):
-        print("Hello World!")
+    def goodbye(self):
+        return f"{self.name} goodbye!"
