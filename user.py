92224d182cc443e7df76acfc6a932e4c5fc09130
Liliya <liliyakhu@ukr.net>
2025-08-24 16:10:46 +0300

    Add image field and goodbye func

--- user.py
+++ user.py
@@ -2,9 +2,10 @@ class User:
     name = "Liliya"
     last_name = "Khudich"
     age = 44
+    image = "some image"
 
     def hello(self):
         print("Hello World!")
 
-
-
+    def goodbye(self):
+        return f"{self.name} {self.last_name} goodbye!"
